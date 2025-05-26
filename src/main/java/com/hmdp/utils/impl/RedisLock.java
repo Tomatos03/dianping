ed35b97128183b5a743a89eb3e8bf200781af325
Tomatos <3333331969@qq.com>
2025-05-26 10:56:31 +0800

    fix: 添加lua脚本保证锁的原子性

--- src/main/java/com/hmdp/utils/impl/RedisLock.java
+++ src/main/java/com/hmdp/utils/impl/RedisLock.java
@@ -2,8 +2,11 @@ package com.hmdp.utils.impl;
 
 import cn.hutool.core.lang.UUID;
 import com.hmdp.utils.ILock;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -17,6 +20,14 @@ public class RedisLock implements ILock {
 
     private final String LOCK_PREFIX = "lock:";
     private final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+
+    private static final DefaultRedisScript<Long> DEFAULT_SCRIPT;
+
+    static {
+        DEFAULT_SCRIPT = new DefaultRedisScript<>();
+        DEFAULT_SCRIPT.setResultType(Long.class);
+        DEFAULT_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+    }
 
     public RedisLock(StringRedisTemplate stringRedisTemplate, String name) {
         this.stringRedisTemplate = stringRedisTemplate;
@@ -38,12 +49,11 @@ public class RedisLock implements ILock {
 
     @Override
     public void unlock() {
-        String lockId = stringRedisTemplate.opsForValue()
-                                      .get(LOCK_PREFIX + name);
-        String currentLockId = ID_PREFIX + Thread.currentThread().getName();
-        if (!currentLockId.equals(lockId)) {
-            return;
-        }
-        stringRedisTemplate.delete(LOCK_PREFIX + name);
+        // 使用 Lua 脚本来保证解锁的原子性
+        stringRedisTemplate.execute(
+                DEFAULT_SCRIPT,
+                Collections.singletonList(LOCK_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getName()
+        );
     }
 }
---
+++ src/main/resources/unlock.lua
@@ -0,0 +1,5 @@
+if ARGV[1] == redis.call('get', KEYS[1]) then
+    return redis.call('del', KEYS[1])
+else
+    return 0
+end
